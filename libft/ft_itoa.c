92c86060b04606fa87cddbbf0734d8dd4db5a640
Carolina <127395560+caroldmg@users.noreply.github.com>
2024-01-20 10:49:42 +0000

    comentarios sobre itoa

---
+++ libft/ft_itoa.c
@@ -0,0 +1,9 @@
+//añadir header 42
+
+char    *ft_itoa(int n)
+{
+    // he oido por ahi que hay que hacer un contador de la cantidad de cifras del n
+    // para saber cuánto espacio reservamos con malloc
+    // algo relacionado con el max y el min int?
+    // será similar al putnbr pero en vez de escribirlo lo guardamos en un char *
+}
