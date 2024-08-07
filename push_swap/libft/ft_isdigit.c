bc3c12c9b1035993f3a238dee82f36474305a2c9
Carolina De Miguel Gonzalez <cde-migu@c1r6s5.42madrid.com>
2024-08-07 18:46:58 +0200

    gestion de errores, liberacion de memoria y algoritmo corregidos

--- push_swap/libft/ft_isdigit.c
+++ push_swap/libft/ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: cde-migu <cde-migu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:40:31 by cde-migu          #+#    #+#             */
-/*   Updated: 2024/02/13 10:56:18 by cde-migu         ###   ########.fr       */
+/*   Updated: 2024/08/07 15:17:11 by cde-migu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,7 @@
 
 int	ft_isdigit(int c)
 {
-	int	isdigit;
-
-	isdigit = 0;
-	if (c >= '0' && c <= '9')
-		isdigit = 1;
-	return (isdigit);
+	return (c >= '0' && c <= '9');
 }
 
 /*
