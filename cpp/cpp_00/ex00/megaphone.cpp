23143e27dd737654851089ade5d59bc4e8e800e2
caroldmg <carolinadmg98@gmail.com>
2025-07-23 16:59:47 +0200

    megaphone refactor

--- cpp/cpp_00/ex00/megaphone.cpp
+++ cpp/cpp_00/ex00/megaphone.cpp
@@ -6,13 +6,34 @@
 /*   By: cde-migu <cde-migu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 12:56:07 by cde-migu          #+#    #+#             */
-/*   Updated: 2025/07/15 15:09:27 by cde-migu         ###   ########.fr       */
+/*   Updated: 2025/07/23 12:37:38 by cde-migu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string.h>
 #include <ctype.h>
+
+void	printUpper(int argc, char **argv)
+{
+	int	i;
+	int	j;
+	int	len;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		len = strlen(argv[i]);
+		while (argv[i][j])
+		{
+			std::cout << (char) toupper(argv[i][j]);
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+}
 
 int	main(int argc, char **argv)
 {
@@ -22,19 +43,7 @@ int	main(int argc, char **argv)
 
 	if (argc > 1)
 	{
-		i = 1;
-		while (i < argc)
-		{
-			j = 0;
-			len = strlen(argv[i]);
-			while (argv[i][j])
-			{
-				std::cout << (char) toupper(argv[i][j]);
-				j++;
-			}
-			i++;
-		}
-		std::cout << std::endl;
+		printUpper(argc, argv);
 		return (0);
 	}
 	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
